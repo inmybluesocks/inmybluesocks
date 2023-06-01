3e471624e9b749504f0098a7d9d9e4808e109f07
chuck <fjc221@yahoo.com>
2023-06-01 01:12:41 -0700

    def int

---
+++ Oject oriented Programming in Python.py
@@ -0,0 +1,26 @@
+# Oject oriented Programming in Python
+
+class Student:
+    def_init_(self, name, age,grade):
+    self.name = __name__
+    self.age = age
+    self.grade = grade # 0 - 100
+
+    def get_grade(self):
+        return self.get_grade
+    
+    class Course:
+        def _init_(self, name, max_studnet):
+            self.name = nameself.max_students = max_students
+            seld.students = []
+
+        def add_student(self. student):
+            if len(self.students) < self.max_students:
+                sef.students.append(student)
+                return True
+            return False
+        
+        def get_average
+
+        
+
